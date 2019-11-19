60fd33175848bbc7830cf974f418b46ac83e40bd
kengsheng <kengsheng@live.com>
2019-11-19 15:28:02 +0800

    Change recaptcha key

--- data/config.js
+++ data/config.js
@@ -1,33 +1,33 @@
 module.exports = {
-	defaultTitle: 'Primo Solutions | Develop Fast Website',
-	logo: '../static/favicon/favicon.ico',
-	author: 'Keng Sheng Lee',
-	url: 'https://myprimosolutions.com',
-	legalName: 'Keng Sheng Lee',
-	defaultDescription:
-		'Website developer, create fast and effective website for businesses without thinning your wallet. Our services include buying domain names, UI/UX design, website development, setting up email for your domain name, construct SEO on your website, and maintain your website.',
-	socialLinks: {
-		github: 'https://github.com/myprimosolutions',
-		facebook: 'https://www.facebook.com/myprimosolutions',
-		google: 'https://plus.google.com/u/0/+myprimosolutions',
-	},
-	googleAnalyticsID: 'UA-148993527-1',
-	themeColor: '#0074D9',
-	backgroundColor: '#0074D9',
-	social: {
-		facebook: 'fb.me/myprimosolutions',
-	},
-	address: {
-		city: 'Kota Bharu',
-		region: 'Kelantan',
-		country: 'Malaysia',
-		zipCode: '16100',
-	},
-	contact: {
-		email: 'info@myprimosolutions.com',
-		phone: '+60179603926',
-	},
-	foundingDate: '2019',
-	recaptcha_key: '6LeWdLoUAAAAAB0KwTRfhLT3_hcYdJ9-XjScfy4h',
-	recaptcha_secret_key: '6LeWdLoUAAAAAFyLOrhZScQPLH05EQMmuesC_jQ5',
+  defaultTitle: 'Primo Solutions | Develop Fast Website',
+  logo: '../static/favicon/favicon.ico',
+  author: 'Keng Sheng Lee',
+  url: 'https://myprimosolutions.com',
+  legalName: 'Keng Sheng Lee',
+  defaultDescription:
+    'Website developer, create fast and effective website for businesses without thinning your wallet. Our services include buying domain names, UI/UX design, website development, setting up email for your domain name, construct SEO on your website, and maintain your website.',
+  socialLinks: {
+    github: 'https://github.com/myprimosolutions',
+    facebook: 'https://www.facebook.com/myprimosolutions',
+    google: 'https://plus.google.com/u/0/+myprimosolutions',
+  },
+  googleAnalyticsID: 'UA-148993527-1',
+  themeColor: '#0074D9',
+  backgroundColor: '#0074D9',
+  social: {
+    facebook: 'fb.me/myprimosolutions',
+  },
+  address: {
+    city: 'Kota Bharu',
+    region: 'Kelantan',
+    country: 'Malaysia',
+    zipCode: '16100',
+  },
+  contact: {
+    email: 'info@myprimosolutions.com',
+    phone: '+60179603926',
+  },
+  foundingDate: '2019',
+  recaptcha_key: '6Lf2cMMUAAAAADvFYOuGgRfGYgtn2JoarTmhlWTa',
+  recaptcha_secret_key: '6Lf2cMMUAAAAAOweCDOHrKJDL8ZiGRIsMrKfO0OA',
 }
